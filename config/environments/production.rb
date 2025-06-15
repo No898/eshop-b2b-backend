95e6a44186b31d3fe6a245b6fb1c9541cea96417
No898 <dinh@seznam.cz>
2025-06-16 00:34:30 +0200

    fix: use SECRET_KEY_BASE env var instead of credentials for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,6 +5,9 @@ require 'active_support/core_ext/integer/time'
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  # Use environment variables for secrets instead of credentials
+  config.secret_key_base = ENV.fetch('SECRET_KEY_BASE', nil)
+
   # Code is not reloaded between requests.
   config.enable_reloading = false
 
