fb0e3834cf6b0aba0ef43216d17bbfc90f8c6073
No898 <dinh@seznam.cz>
2025-06-15 21:37:49 +0200

    Fix routes.rb linting issues

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,10 @@
 
 Rails.application.routes.draw do
   post '/graphql', to: 'graphql#execute'
-  
+
   # GraphiQL interface for development
-  if Rails.env.development?
-    mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: '/graphql'
-  end
-  
+  mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: '/graphql' if Rails.env.development?
+
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
