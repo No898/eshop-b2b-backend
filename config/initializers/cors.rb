6e3f9f74f2c5c4599150713641a0060f0814eece
No898 <dinh@seznam.cz>
2025-06-17 19:29:15 +0200

    feat: enable CORS for frontend connection

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,12 +7,18 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins "example.com"
-#
-#     resource "*",
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    # Povolit všechny origins pro development a testování
+    # V produkci byste měli specifikovat konkrétní domény
+    origins '*'
+
+    # Pro produkci použijte konkrétní domény:
+    # origins ['https://your-frontend-domain.com', 'https://localhost:3000', 'http://localhost:3000']
+
+    resource '*',
+             headers: :any,
+             methods: %i[get post put patch delete options head],
+             credentials: false
+  end
+end
